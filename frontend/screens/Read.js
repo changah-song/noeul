814a4a71d29a545e3b01392342a747269f8b2978
Paul Song <paulsong00@uni.minerva.edu>
2026-01-29 10:44:12 +0900

    no loading circle anymore when no books are present

--- frontend/screens/Read.js
+++ frontend/screens/Read.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, SafeAreaView, StyleSheet } from 'react-native';
+import { View, SafeAreaView, StyleSheet, Text } from 'react-native';
 
 import { Reader, ReaderProvider, useReader } from '@epubjs-react-native/core';
 import { useFileSystem } from '@epubjs-react-native/expo-file-system';
@@ -60,6 +60,14 @@ const BottomSection = ({books, setBooks, currentBook, setHighlightedWord}) => {
 
     const initialLocation = books.find(book => book.uri === currentBook)?.location;
 
+    if (!currentBook) {
+        return (
+            <View style={styles.noBookContainer}>
+                <Text style={styles.noBookText}>No book selected</Text>
+            </View>
+        );
+    }
+
     return  (
         <Reader
             src={currentBook}
@@ -89,6 +97,15 @@ const styles = StyleSheet.create({
     },
     reader: {
         flex: 0.82,
+    },
+    noBookContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    noBookText: {
+        fontSize: 18,
+        color: '#6e7b8b',
     }
 });
 
